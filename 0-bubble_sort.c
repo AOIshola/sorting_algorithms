bc40bce76b32d3667e995936d9d390629611c50b
Armolas <armolas06@gmail.com>
2023-11-21 00:09:30 +0000

    bubble_sort and insertion_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers using the bubble sort algorithm
+ * @array: the array to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int flag, temp;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		flag = 0;
+		for (j = 0 ; j < size - (1 + i) ; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+}
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+void swap_list_nodes(listint_t **head, listint_t *node_a, listint_t *node_b);
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers using
+ * insertion sort algorithm
+ * @list: the list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *inner = *list;
+
+	while (inner->next)
+	{
+		if (inner->n > inner->next->n)
+		{
+			swap_list_nodes(list, inner, inner->next);
+			print_list(*list);
+			inner = *list;
+		}
+		else
+			inner = inner->next;
+	}
+}
+/**
+ * swap_list_nodes - swap two nodes of a doubly linked list
+ * @head: head of the list
+ * @node_a: first node
+ * @node_b: second node
+ */
+void swap_list_nodes(listint_t **head __attribute__((unused)),
+		listint_t *node_a, listint_t *node_b)
+{
+	node_a->next = node_b->next;
+	node_b->prev = node_a->prev;
+	if (node_a->prev)
+		node_a->prev->next = node_b;
+	else
+		*head = node_b;
+	node_a->prev = node_b;
+	if (node_b->next)
+		node_b->next->prev = node_a;
+	node_b->next = node_a;
+}
